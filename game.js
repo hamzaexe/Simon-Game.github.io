b5041cd4bef3db77c114d4e99289dd25b2313090
hamza nadeem <hnb351@gmail.com>
2021-04-25 09:24:07 +0100

    add level and start function

--- game.js
+++ game.js
@@ -1,43 +1,13 @@
-// var userClickedPattern=[];
-// var buttonColours = ["red", "blue", "green", "yellow"];
-// var gamePattern = [];
-// function playSound(name)
-// {
-//   $(".btn").click(function(){
-//       name=this.id;
-//       var audio = new Audio("sounds/"+name+".mp3");
-//       audio.play();
-//   });
-// }
-// playSound(name);
-// function nextSequence() {
-//   var randomNumber = Math.floor(Math.random() * 4);
-//   var randomChosenColour = buttonColours[randomNumber];
-//   gamePattern.push(randomChosenColour);
-//   //1. Use jQuery to select the button with the same id as the randomChosenColour
-//   //2. Use Google/Stackoverflow to figure out how you can use jQuery to animate a flash to the button selected in step 1.
-//   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-//   //3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
-//   // var audio = new Audio("sounds/blue.mp3");
-//   // audio.play();
-//   playSound(randomChosenColour);
-// }
-// nextSequence();
-//
-// $(".btn").click(function(){
-//     var userChosenColour=this.id;
-//     userClickedPattern.push(userChosenColour);
-//     console.log(userClickedPattern);
-// });
-
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
-
+var level=0;
+var gameStatus=0;
 $(".btn").click(function() {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
+  nextSequence();
 });
 
 function playSound(name) {
@@ -50,8 +20,11 @@ function nextSequence() {
   gamePattern.push(randomChosenColour);
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
+
+  $("h1").text("level"+" "+level);
+  level+=1;
 }
-nextSequence();
+
 function animatePress(currentColour){
   $(".btn").click(function(){
     currentColour = $(this).attr("id");
@@ -60,5 +33,16 @@ function animatePress(currentColour){
         $("#" + currentColour).removeClass("pressed");
     }, 100);
   });
+
 }
 animatePress();
+$(document).on('keypress',function(e) {
+    if(e.key === "a" && gameStatus==0) {
+      $("h1").text("level"+" "+level);
+        nextSequence();
+        gameStatus=1;
+
+
+
+    }
+});
